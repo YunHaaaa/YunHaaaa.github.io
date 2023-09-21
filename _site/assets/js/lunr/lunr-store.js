240d1956b2edad46fcca172faba82e50a514b035
YunHaaaa <qlxqlrt2012@gmail.com>
2023-09-21 19:13:27 +0900

    Feat: Change theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Edge TPU 모델 설계 ]  #1 Google Coral 공식문서",
+        "excerpt":"Google Coral 공식문서를 참조해서 Edge TPU에서 구동시킬 Tensorflow 모델을 설계하고자 합니다. ​ Edge Tpu Edge TPU에 대해 완전히 8비트 양자화된 TensorFlow Lite 모델만 지원합니다. TensorFlow Lite로 직접 모델을 훈련할 수는 없으며, 대신 TensorFlow 파일(.pb 파일)을 TensorFlow Lite 파일(.tflite 파일)로 변환해야 합니다. ​ ​ 전이 학습 전이 학습은 직접 모델을 구축하고...","categories": ["deep","learning"],
+        "tags": [],
+        "url": "/deep/learning/edgetpu-1/",
+        "teaser": null
+      }]
